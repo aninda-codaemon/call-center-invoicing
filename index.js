c53192b842f0488524f462438caa02bf47031b85
Aninda Kar <aninda.kar@outlook.com>
2019-09-03 18:17:23 +0530

    react client build config static

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(express.json({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   console.log('Server root!!');
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/users', require('./routes/users'));
+app.use('/api/agents', require('./routes/users'));
 app.use('/api/order', require('./routes/invoice'));
 
 const PORT = process.env.PORT || 5000;

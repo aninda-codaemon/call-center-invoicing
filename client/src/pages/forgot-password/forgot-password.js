0fad59a0ed7f291c36db399823aacbbe18b7c052
soumitra-codaemon <soumitra.sahoo@codaemonsoftwares.com>
2019-09-10 21:13:00 +0530

    working on cost calculation API

--- client/src/pages/forgot-password/forgot-password.js
+++ client/src/pages/forgot-password/forgot-password.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/img/logo.png";
 import { Button } from "react-bootstrap";

4310d958f7d800a2bb2da388a365afd987a7064e
Aninda Kar <aninda.kar@outlook.com>
2019-09-12 08:12:37 +0530

    Add check for unreturned msa

--- routes/invoice.js
+++ routes/invoice.js
@@ -26,55 +26,59 @@ router.post('/pricing', authMiddleware, async (req, res) => {
   const service_type = req.body.servicetype;
   const additional_charges = parseFloat(req.body.addlcharges) || 0.00;
   const timestamp = req.body.timestamp;
-
+  console.log(req.body);
   const msa = await InvoiceModel.getMsaFromZip(origin_zipcode);
 
   if (msa.error) {
     return res.status(500).json({ errors: [{ msg: 'Internal server error!' }] });
   } else {
-    const msa_price = await InvoiceModel.getPriceForMSA(msa.result[0]['msa_id']);
-
-    if (msa_price.error) {
-      return res.status(500).json({ errors: [{ msg: 'Internal server error!' }] });
-    } else {
-      const pricing = msa_price.result[0];
-      const over_miles_price = parseFloat(pricing['per_mile_rate_over_ten_miles'].replace('$', ''));
-      const chargable_miles = total_distance - 10;
-      const service_charges = 3.5 / 100;
-      let base_price = 0.00;
-      let net_price = 0.00;
-      let total_price = 0.00;
-
-      if (service_type === 'towing') {
-        // Base rate calculation on location timestamp pending. 
-        base_price = parseFloat(pricing['retail_tow_rate'].replace('$', ''));
-
-        // Price calculation
-        if (total_distance > 10) {
-          net_price = base_price + ((total_distance - 10) * over_miles_price);
+    if (msa.result.length > 0) {
+      const msa_price = await InvoiceModel.getPriceForMSA(msa.result[0]['msa_id']);
+      if (msa_price.error) {
+        return res.status(500).json({ errors: [{ msg: 'Internal server error!' }] });
+      } else {
+        const pricing = msa_price.result[0];
+        const over_miles_price = parseFloat(pricing['per_mile_rate_over_ten_miles'].replace('$', ''));
+        const chargable_miles = total_distance - 10;
+        const service_charges = 3.5 / 100;
+        let base_price = 0.00;
+        let net_price = 0.00;
+        let total_price = 0.00;
+  
+        if (service_type === 'towing') {
+          // Base rate calculation on location timestamp pending. 
+          base_price = parseFloat(pricing['retail_tow_rate'].replace('$', ''));
+  
+          // Price calculation
+          if (total_distance > 10) {
+            net_price = base_price + ((total_distance - 10) * over_miles_price);
+          } else {
+            net_price = base_price;
+          }
         } else {
+          // Base rate calculation on location timestamp pending.
+          base_price = parseFloat(pricing['retail_light_service_rate'].replace('$', ''));
           net_price = base_price;
         }
-      } else {
-        // Base rate calculation on location timestamp pending.
-        base_price = parseFloat(pricing['retail_light_service_rate'].replace('$', ''));
-        net_price = base_price;
+  
+        net_price = net_price + additional_charges;
+        total_price = parseFloat((net_price + (net_price * service_charges)).toFixed(2));
+  
+        return res.status(200).json({
+          errors: [], data: {
+            msg: 'Total price',
+            base_price,
+            total_price,
+            net_price,
+            service_charges,
+            system: pricing['system']
+          }
+        });
       }
-
-      net_price = net_price + additional_charges;
-      total_price = parseFloat((net_price + (net_price * service_charges)).toFixed(2));
-
-      return res.status(200).json({
-        errors: [], data: {
-          msg: 'Total price',
-          base_price,
-          total_price,
-          net_price,
-          service_charges,
-          system: pricing['system']
-        }
-      });
+    } else {
+      return res.status(400).json({ errors: [{ msg: 'No service for this location' }] });
     }
+    
   }
 });
 

a1e4aee4b00718d68ecba6270d63f36c18bb7654
Aninda Kar <aninda.kar@outlook.com>
2019-09-19 19:11:10 +0530

    testing

--- client/src/context/Types.js
+++ client/src/context/Types.js
@@ -11,4 +11,4 @@ export const USER_FETCHPAGE = 'USER_FETCHPAGE';
 export const USER_SEARCHTERM = 'USER_SEARCHTERM';
 export const USER_SORTORDER = 'USER_SORTORDER';
 export const USER_ERROR = 'USER_ERROR';
-export const SERVER_URL = 'http://localhost:5000'; //'http://18.217.104.6';
+export const SERVER_URL = 'http://18.217.104.6'; //'http://localhost:5000'; //'http://18.217.104.6';
